740c493a74bbe3b559a70ab9f27933c5d07d9ade
Антон Ледовский <127601708+Aurum68@users.noreply.github.com>
2025-01-27 17:36:18 +0200

    Rewrite Task1 Lab2

--- LaboratoryWork2/Task1.py
+++ LaboratoryWork2/Task1.py
@@ -1,39 +1,31 @@
-alphabet = 'ЧЕРЕСПОЛОСИЦА'
-words_len = 6
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
 
-words = set()
+def count_permutations(word, length):
+    from collections import Counter
+    
+    # Подсчет количества каждой буквы
+    letter_count = Counter(word)
+    total_letters = sum(letter_count.values())
+    
+    # Если длина больше общего количества букв, вернуть 0
+    if length > total_letters:
+        return 0
+    
+    # Считаем количество перестановок
+    total_permutations = factorial(total_letters) // factorial(total_letters - length)
+    
+    for count in letter_count.values():
+        total_permutations //= factorial(count)
+    
+    return total_permutations
 
-for a in range(len(alphabet)):
-    if a != len(alphabet) - 1:
-        remain1 = alphabet[:a] + alphabet[a+1:]
-    else:
-        remain1 = alphabet[:a]
-
-    for b in range(len(remain1)):
-        if b != len(remain1) - 1:
-            remain2 = remain1[:b] + remain1[b+1:]
-        else:
-            remain2 = remain1[:b]
-
-        for c in range(len(remain2)):
-            if c != len(remain2) - 1:
-                remain3 = remain2[:c] + remain2[c+1:]
-            else:
-                remain3 = remain2[:c]
-
-            for d in range(len(remain3)):
-                if d != len(remain3) - 1:
-                    remain4 = remain3[:d] + remain3[d+1:]
-                else:
-                    remain4 = remain4[:d]
-
-                for e in range(len(remain4)):
-                    if e != len(remain4) - 1:
-                        remain5 = remain4[:e] + remain4[e+1:]
-                    else:
-                        remain5 = remain1[:e]
-
-                    for f in range(len(remain5)):
-                        word = alphabet[a] + remain1[b] + remain2[c] + remain3[d] + remain4[e] + remain5[f] #+ remain6[g]
-                        words.add(word)                          
-print(len(words))
+word = "ЧЕРЕСПОЛОСИЦА"
+length = 6
+result = count_permutations(word, length)
+print(f"Количество различных слов длиной {length}: {result}")
