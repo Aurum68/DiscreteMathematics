fd79712ac33f4659689747e54a5ceee742dfcae0
Антон Ледовский <127601708+Aurum68@users.noreply.github.com>
2025-02-19 19:53:51 +0200

    Add Task1.py, Task2.py Finish Task3.py Set text.txt

---
+++ LaboratoryWork4/Task1.py
@@ -0,0 +1,50 @@
+import math
+from collections import Counter
+
+with open('text.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+    f.close()
+    # Подсчет частоты букв
+    char_freq = Counter(text)
+
+    # Подсчет частоты пар букв
+    pair_freq = Counter([text[i:i + 2] for i in range(len(text) - 1)])
+
+def statistics():
+    global text, char_freq, pair_freq
+
+    # Выведем 10 самых частых символов и 10 самых частых пар
+
+    top_chars = char_freq.most_common(10)
+
+    top_pairs = pair_freq.most_common(10)
+
+    print(top_chars, top_pairs)
+
+
+    with open('statistics_mono.txt', 'w', encoding='utf-8') as f:
+        for k, v in char_freq.items():
+            f.write(k + '\t' + str(v) + '\n')
+        f.close()
+
+    with open('statistics_double.txt', 'w', encoding='utf-8') as f:
+        for k, v in pair_freq.items():
+            f.write(k + '\t' + str(v) + '\n')
+        f.close()
+
+
+def Shennon():
+    global text
+    shennon_information = -sum([(char_freq[i]/len(text) * math.log2(char_freq[i]/len(text))) for i in char_freq])
+
+    with open('results.txt', 'w', encoding='utf-8') as f:
+        f.write("По формуле Шеннона " + str(shennon_information) + ' бит\n')
+        f.close()
+
+
+def uniform_code():
+    global text, char_freq
+    original_size = sum(char_freq.values()) * 6
+    with open('results.txt', 'a', encoding='utf-8') as f:
+        f.write("Равномерный код :" + str(original_size) + ' бит\n')
+    return original_size
---
+++ LaboratoryWork4/Task3.py
@@ -0,0 +1,64 @@
+import math
+
+import Task1
+
+Task1.statistics()
+
+codes = dict()
+with open('statistics_mono.txt', 'r') as f:
+    counter = 0
+    for line in f:
+        key, value = line.split('\t')
+        codes[key] = counter
+        counter += 1
+    f.close()
+print(codes)
+length = len(codes)
+
+with open('text.txt', 'r') as f:
+    last_read_symbol = f.read(1)
+    end = False
+
+    while True:
+        if last_read_symbol == '' or last_read_symbol == '\n':
+            break
+        current_line = last_read_symbol
+        longest_prefix = ''
+
+        if current_line in list(codes.keys()):
+            while current_line in list(codes.keys()):
+                longest_prefix = current_line
+                last_read_symbol = f.read(1)
+                if last_read_symbol == '':
+                    break
+                counter += 1
+                current_line += last_read_symbol
+
+
+        if current_line not in list(codes.keys()):
+            if len(longest_prefix) > 0:
+                codes[current_line] = list(codes.keys()).index(longest_prefix)
+            else:
+                codes[current_line] = len(list(codes.keys()))
+
+print(codes)
+
+coded_text = [list(codes.values())[i] for i in range(length, len(list(codes.values())))]
+print(coded_text)
+
+bin_min_len = math.ceil(math.log(length, 2))
+
+result = ''
+for code in coded_text:
+    if 0 <= code <= length - 1:
+        print(format(code, f'#0{bin_min_len + 2}b')[2:], end='.')
+        result += format(code, f'#0{bin_min_len + 2}b')[2:]
+    else:
+        print(bin(code)[2:], end='.')
+        result += bin(code)[2:]
+
+with open('results.txt', 'a', encoding='utf-8') as f:
+    f.write("LZW: " + str(len(result)) + ' бит\n')
+
+if __name__ == '__main__':
+    pass
